4553f98a9c49f0df25802446ed1623349cdd284b
zhaomj <zhaomj2@huntkey.net>
2018-02-14 11:25:10 +0800

    调整首页样式

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
 </style>
 <template>
     <div class="layout" id="app">
-        <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
+        <!-- <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
 
-        </Sider>
-        <Layout :style="{marginLeft: '200px'}">
+        </Sider> -->
+        <Layout :style="{marginLeft: '0'}">
             <Content :style="{padding: '0 16px 16px'}">
                 <router-view/>
             </Content>
