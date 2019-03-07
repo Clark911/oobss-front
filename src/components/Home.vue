d035bde2d370432f65975498b3a9aa0b0f28416b
clark911 <zmj911@sina.com>
2019-03-08 00:54:42 +0800

    修改样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,150 +1,53 @@
 <style scoped>
-.task{
-  min-width: 200px;
-  width: 100;
-  min-height: 26px;
-  padding: 2px;
-  overflow: hidden;
-}
-.task:hover{
-  background-color: beige;
-}
-.task-text{
-  float: left;
-  max-width: 800px;
-  min-width: 180px;
-  word-break: break-all;
-  -webkit-line-clamp: 1;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  white-space: normal;
-}
-.btn-finish{
-  float: right;
-  /* padding-top: 4px; */
-  /* border: 1px solid black; */
-}
+
 </style>
 
 <template>
-<el-collapse accordion value="todo" :style="{height:'100%'}">
-  <el-collapse-item name="later">
-    <template slot="title">
-      拖后<i class="header-icon el-icon-info"></i>
-    </template>
-    <ul>
-      <li v-for="item in lastTasks" v-bind:key="item.id">
+  <el-collapse accordion value="todo" :style="{height:'100%'}">
+    <el-collapse-item title="拖后" name="later">
+      <el-table :data="lastTasks" style="width: 100%" :showHeader=false>
+        <el-table-column
+          label="任务"
+          prop="name">
+        </el-table-column>
+        <el-table-column fixed="right" width="180">
+          <template slot-scope="item">
+            <el-button v-on:click="backTask(item.row.id)" size="small">Redo</el-button>
+            <el-button v-on:click="deleteTask(item.row.id)" size="small">Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-collapse-item>
+    <el-collapse-item title="今日待办" name="todo">
+      <el-table :data="currentTasks" style="width: 100%" :showHeader=false>
+        <el-table-column
+          label="任务"
+          prop="name">
+        </el-table-column>
+        <el-table-column fixed="right" width="180">
+          <template slot-scope="item">
+            <el-button size="small" v-bind:loading="btnLodingStatus[item.row.id]"  v-on:click="fixTask(item.row.id)">Done</el-button>
+            <el-button size="small" v-bind:loading="btnLodingStatus[item.row.id]" v-on:click="deleteTask(item.row.id)">Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-collapse-item>
+    <el-collapse-item title="完成了" name="done">
+      <ul>
+      <li v-for="item in finishedTasks" v-bind:key="item.id">
         <div class="task" v-on:mouseenter="showBackBtn">
           <div class="task-text">
-            {{ item.name }}({{item.time}})
+            {{ item.name }}
           </div>
           <div class="btn-finish">
-            <el-button v-on:click="backTask(item.id)">Redo</el-button>
-            <el-button v-on:click="deleteTask(item.id)"></el-button>
+            <el-button v-bind:loading="btnLodingStatus[item.id]" v-on:click="backTask(item.id)">Redo</el-button>
+            <el-button v-bind:loading="btnLodingStatus[item.id]" v-on:click="deleteTask(item.id)"></el-button>
           </div>
         </div>
-      </li>
-    </ul>
-  </el-collapse-item>
-  <el-collapse-item title="今日待办" name="todo">
-    <ul>
-      <li v-for="item in currentTasks" v-bind:key="item.id">
-          <div class="task" v-on:mouseenter="showDoneBtn">
-              <div class="task-text">
-                {{ item.name }}
-              </div>
-              <div class="btn-finish">
-                <el-button v-bind:loading="btnLodingStatus[item.id]"  v-on:click="fixTask(item.id)">Done</el-button>
-                <el-button v-bind:loading="btnLodingStatus[item.id]" v-on:click="deleteTask(item.id)"></el-button>
-              </div>
-          </div>
       </li>
       </ul>
-  </el-collapse-item>
-  <el-collapse-item title="完成了" name="done">
-    <ul>
-    <li v-for="item in finishedTasks" v-bind:key="item.id">
-      <div class="task" v-on:mouseenter="showBackBtn">
-        <div class="task-text">
-            {{ item.name }}
-        </div>
-        <div class="btn-finish">
-          <el-button v-bind:loading="btnLodingStatus[item.id]" v-on:click="backTask(item.id)">Redo</el-button>
-          <el-button v-bind:loading="btnLodingStatus[item.id]" v-on:click="deleteTask(item.id)"></el-button>
-        </div>
-      </div>
-    </li>
-    </ul>
-  </el-collapse-item>
-</el-collapse>
-  <!-- <div>
-    <Card>
-      <div style="height: 200px">
-        <h1>拖后</h1>
-        <Scroll :on-reach-bottom="handleReachBottom" height="150">
-          <ul>
-            <li v-for="item in lastTasks" v-bind:key="item.id">
-              <div class="task" v-on:mouseenter="showBackBtn">
-                <div class="task-text">
-                  {{ item.name }}({{item.time}})
-                </div>
-                <div class="btn-finish">
-                  <Button shape="circle" icon="reply" size="small" v-on:click="backTask(item.id)">Redo</Button>
-                  <Button type="dashed" shape="circle" icon="trash-a" size="small" v-on:click="deleteTask(item.id)"></Button>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </Scroll>
-      </div>
-    </Card>
-
-    <Card>
-      <div style="height: 400px">
-        <h1>今日待办</h1>
-        <Scroll :on-reach-bottom="handleReachBottom">
-            <ul>
-            <li v-for="item in currentTasks" v-bind:key="item.id">
-                <div class="task" v-on:mouseenter="showDoneBtn">
-                    <Tooltip max-width="200" placement="top" v-bind:content="item.name">
-                        <div class="task-text">
-                          {{ item.name }}
-                        </div>
-                    </Tooltip>
-                    <div class="btn-finish">
-                      <Button shape="circle" v-bind:loading="btnLodingStatus[item.id]" icon="checkmark-round" size="small" v-on:click="fixTask(item.id)">Done</Button>
-                      <Button type="dashed" shape="circle" icon="trash-a" size="small" v-bind:loading="btnLodingStatus[item.id]" v-on:click="deleteTask(item.id)"></Button>
-                    </div>
-                </div>
-            </li>
-            </ul>
-        </Scroll>
-        <Input placeholder="添加任务······" v-on:on-enter="putTask" v-model="taskName"></Input>
-      </div>
-    </Card>
-
-    <Card>
-      <div style="height: 200px">
-        <h1>完成了</h1>
-        <Scroll :on-reach-bottom="handleReachBottom" height="150">
-          <ul>
-          <li v-for="item in finishedTasks" v-bind:key="item.id">
-            <div class="task" v-on:mouseenter="showBackBtn">
-              <div class="task-text">
-                  {{ item.name }}
-              </div>
-              <div class="btn-finish">
-                <Button shape="circle" icon="reply" size="small" v-bind:loading="btnLodingStatus[item.id]" v-on:click="backTask(item.id)">Redo</Button>
-                <Button type="dashed" shape="circle" icon="trash-a" size="small" v-bind:loading="btnLodingStatus[item.id]" v-on:click="deleteTask(item.id)"></Button>
-              </div>
-            </div>
-          </li>
-          </ul>
-        </Scroll>
-      </div>
-    </Card>
-  </div> -->
+    </el-collapse-item>
+  </el-collapse>
 </template>
 <script>
 export default {
